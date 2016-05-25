6cc4f03c29b33bc1d7b4f72484e87186ea076bc0
janushka <marc.njoku@icloud.com>
2016-05-25 14:57:07 +0200

    Continued with Web Server.

--- fetchall.go
+++ fetchall.go
@@ -1,4 +1,5 @@
 // Fetchall fetches URLs in parallel and reports their times and sizes.
+/*
 package main
 
 import (
@@ -44,4 +45,4 @@ func fetch(url string, ch chan <- string) {
 	}
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
-}
+}*/
